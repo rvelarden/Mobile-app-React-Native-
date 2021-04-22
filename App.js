87cfd9012195a4f57677f8d642f1d063e5ec4886
github username <github email address>
2021-04-22 14:17:19 -0400

    Added dependencies

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Navigation from "./navigation";
+import { Block, Text } from "./components";
+
+export default class App extends React.Component {
+  // add fonts support
+  state = {
+    isLoadingComplete: false
+  };
+
+  handleResourcesAsync = async () => {
+    return Promise.all([
+      Font.loadAsync({
+        "Rubik-Regular": require("./assets/fonts/Rubik-Regular.ttf"),
+        "Rubik-Black": require("./assets/fonts/Rubik-Black.ttf"),
+        "Rubik-Bold": require("./assets/fonts/Rubik-Bold.ttf"),
+        "Rubik-Italic": require("./assets/fonts/Rubik-Italic.ttf"),
+        "Rubik-Light": require("./assets/fonts/Rubik-Light.ttf"),
+        "Rubik-Medium": require("./assets/fonts/Rubik-Medium.ttf")
+      })
+    ]);
+  };
+
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this.handleResourcesAsync}
+          onError={error => console.warn(error)}
+          onFinish={() => this.setState({ isLoadingComplete: true })}
+        />
+      );
+    }
+
+    return (
+      <Block>
+        <Navigation />
+      </Block>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
+
